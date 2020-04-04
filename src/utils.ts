38a0c5b04714b6218c577e3d279239c541d2bc8b
Timur Manyanov <gtibalt@gmail.com>
2020-04-05 00:41:28 +0200

    Fix time

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const hoursSince = (time: string): string => {
   const hours = (Date.now() - new Date(time).getTime()) / 1000 / 60 / 60;
   const h = Math.floor(hours);
-  const m = Math.floor((hours % h) * 60);
+  const m = Math.floor((h ? hours % h : hours) * 60);
 
   return `${h}:${m}`;
 };
