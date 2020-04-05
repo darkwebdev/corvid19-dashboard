1d342ff5d48a0e007f2aac9112b1f90af9902383
Timur Manyanov <gtibalt@gmail.com>
2020-04-05 02:57:21 +0200

    Simplify update time

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
 export const hoursSince = (time: string): string => {
   const hours = (Date.now() - new Date(time).getTime()) / 1000 / 60 / 60;
   const h = Math.floor(hours);
-  const m = Math.floor((h ? hours % h : hours) * 60);
 
-  return `${h}:${m}`;
+  return String(h);
 };
